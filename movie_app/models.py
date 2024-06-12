55ac8804b081edcd265bf94d083f06cdc8fce66b
Wizqqr <baban1979molotov@gmail.com>
2024-06-12 13:31:16 +0600

    Average Rate, Movie_Count by Director, Movie with Reviews

--- movie_app/models.py
+++ movie_app/models.py
@@ -1,10 +1,11 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
 class Director(models.Model):
     name = models.CharField(max_length=200)
 
     def __str__(self):
-        return self.name
+        return  self.name
 
     class Meta:
         verbose_name = "Director"
@@ -25,6 +26,7 @@ class Movie(models.Model):
 class Review(models.Model):
     text = models.CharField(max_length=100)
     movie = models.ForeignKey(Movie, related_name='reviews', on_delete=models.CASCADE)
+    stars = models.PositiveIntegerField(default=5, validators=[MinValueValidator(1), MaxValueValidator(5)], null=True)
 
     def __str__(self):
         return self.text
--- movie_app/views.py
+++ movie_app/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Count, Avg
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
@@ -6,17 +7,16 @@ from .models import Director, Movie, Review
 
 @api_view(['GET'])
 def director_list_api_view(request):
-    data = Director.objects.all()
-    list_ = DirectorSerializer(data, many=True).data
-    return Response(data=list_)
-
+    directors = Director.objects.annotate(movies_count=Count('movies'))
+    serialized_data = DirectorSerializer(directors, many=True).data
+    return Response(data=serialized_data)
 
 @api_view(['GET'])
 def director_detail_api_view(request, id):
     try:
-        director = Director.objects.get(id=id)
+        director = Director.objects.annotate(movies_count=Count('movies')).get(id=id)
     except Director.DoesNotExist:
-        return Response(data={'error':'Director not found'},status=status.HTTP_404_NOT_FOUND)
+        return Response(data={'error': 'Director not found'}, status=status.HTTP_404_NOT_FOUND)
     data = DirectorSerializer(director).data
     return Response(data=data)
 
@@ -31,9 +31,17 @@ def movie_detail_api_view(request, id):
     try:
         movie = Movie.objects.get(id=id)
     except Movie.DoesNotExist:
-        return Response(data={'error':'Movie not found'},status=status.HTTP_404_NOT_FOUND)
+        return Response(data={'error': 'Movie not found'}, status=status.HTTP_404_NOT_FOUND)
     data = MovieSerializer(movie).data
     return Response(data=data)
+
+@api_view(['GET'])
+def movie_reviews_api_view(request):
+    movies = Movie.objects.annotate(
+        avg_rating=Avg('reviews__stars')
+    ).prefetch_related('reviews')
+    serialized_data = MovieSerializer(movies, many=True).data
+    return Response(data=serialized_data)
 
 @api_view(['GET'])
 def review_list_api_view(request):
@@ -46,6 +54,6 @@ def review_detail_api_view(request, id):
     try:
         review = Review.objects.get(id=id)
     except Review.DoesNotExist:
-        return Response(data={'error':'Review not found'},status=status.HTTP_404_NOT_FOUND)
+        return Response(data={'error': 'Review not found'}, status=status.HTTP_404_NOT_FOUND)
     data = ReviewSerializer(review).data
     return Response(data=data)
